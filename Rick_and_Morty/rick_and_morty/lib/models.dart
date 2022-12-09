857252dbcfcc3f51d6de7678aa8afa079c66984c
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2022-12-08 19:00:28 -0500

    Finishing flutter project

--- Rick_and_Morty/rick_and_morty/lib/models.dart
+++ Rick_and_Morty/rick_and_morty/lib/models.dart
@@ -1,10 +1,10 @@
 class Character {
-  String? name;
-  String? img;
-  int? id;
+  String name = '';
+  String img = '';
+  int id = 0;
+
+  Character(this.name, this.img, this.id);
   Character.fromJson(Map<String, dynamic> json) {
-    name = json['name'];
-    img = json['img'];
-    id = json['id'];
+    Character(name = json['name'], img = json['image'], id = json['id']);
   }
 }
